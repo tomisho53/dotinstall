26bcbe6ddd1453b3e4114e4561736895caf1bacd
Hajime Egawa <hajime_egawa@mail.ryoyu.co.jp>
2019-10-31 15:48:32 +0900

    Initial commit

--- 50.Office/Grid/js/main.js
+++ 50.Office/Grid/js/main.js
@@ -2,6 +2,30 @@
 
 {
 
+  const MemberDatas =
+  [
+    {
+      "img1": "img/member2.png",
+      "img2": "img/member3.png",
+      "nickname": "ＫＢ",
+      "name": "久保　直隆<span> - kubo naotaka - </span>",
+      "qa1": "ベスト：04:40:07",
+      "qa2": "大会：富山マラソン2019",
+      "qa3": "好きなお酒：ハイボール！！",
+    },
+    {
+      "img1": "img/member2.png",
+      "img2": "img/member3.png",
+      "nickname": "こみやくん",
+      "name": "小宮　ＸＸ<span> - komiya xxxx - </span>",
+      "qa1": "ベスト：03:52:07",
+      "qa2": "大会：板橋マラソン2019",
+      "qa3": "好きなお酒：日本酒！！",
+    },
+  ]
+
+  let member_no = 0;
+
   // memberクラス
   class Member {
     constructor() {
@@ -13,10 +37,10 @@
       const div = document.createElement('div');
       div.classList.add('img-box');
       this.img_1 = document.createElement('img');
-      this.img_1.src = "img/member2.png";
+      this.img_1.src = MemberDatas[member_no]['img1'];
 
       this.img_2 = document.createElement('img');
-      this.img_2.src = "img/member3.png";
+      this.img_2.src = MemberDatas[member_no]['img2'];
       this.img_2.classList.add('active');
 
       div.appendChild(this.img_1);
@@ -25,22 +49,22 @@
 
       // ニックネーム
       const h1 = document.createElement('h1');
-      h1.innerText = "ＫＢ";
+      h1.innerText = MemberDatas[member_no]['nickname'];
       section.appendChild(h1);
 
       // 名前
       const h2 = document.createElement('h2');
-      h2.innerHTML = "久保　直隆<span> - kubo naotaka - </span>";
+      h2.innerHTML = MemberDatas[member_no]['name'];
       section.appendChild(h2);
 
       // 自己紹介
       const ul = document.createElement('ul');
       const li1 = document.createElement('li');
-      li1.innerText = "ベスト：04:40:07";
+      li1.innerText = MemberDatas[member_no]['qa1'];
       const li2 = document.createElement('li');
-      li2.innerText = "大会：富山マラソン2019";
+      li2.innerText = MemberDatas[member_no]['qa2'];
       const li3 = document.createElement('li');
-      li3.innerText = "好きなお酒：ハイボール";
+      li3.innerText = MemberDatas[member_no]['qa3'];
       ul.appendChild(li1);
       ul.appendChild(li2);
       ul.appendChild(li3);
@@ -54,23 +78,11 @@
   }
 
   // Memberクラスを新規生成する
-  const members = [
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-    new Member(),
-  ];
+  const members = [];
+
+  for (  var i = 0;  i < MemberDatas.length;  i++  ) {
+    member_no = i;
+    members.push(new Member());
+   }
 
 }
